c08c439898c788b7c4c4fdd10ab423e7c2d5e0cb
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-21 18:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    var paths = document.querySelectorAll("#developmentPaths li");
+    paths.forEach(function(path) {
+        path.textContent += " development";
+    });
+});
